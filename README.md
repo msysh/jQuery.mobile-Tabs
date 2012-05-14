71cbd71d4e5d5fc4beb2269133871f7dd0c4f394
msysh <msysh-development@msysh.com>
2012-05-14 13:21:27 +0900

    Tab bar not have to be located in the header, and change style.

--- README.md
+++ README.md
@@ -13,31 +13,31 @@ with the following **caveats**:
 
 - Does not load any content via AJAX
 - Does not load any different actual page parts, and depends on user doing some css in order to make some block-level items display as they would normally in jQuery.mobile
-- Tab bar must be located in the header
-- Content panels must be inside of the content area
+- **Tab bar not have to be located in the header**
+- Content panels not have to be inside of the content area
 - I hope to improve this sometime, or maybe the jQuery.mobile team will be able to use what I've got here to make something awesomer.
 
 This being said, here's a sample of the usage:
 
 	<div data-role="page" id="page-title">
-		<div data-role="header">
-			<h1><%= @page_title %></h1>
-			<div data-role="tabs">
-				<ul>
-					<li><a href="#tab-1" class="">Tab 1</a></li>
-					<li><a href="#tab-2" class="">Tab 2</a></li>
-				</ul>
-			</div>
-		</div>
 
-		<div data-role="content">
-			<ul id="tab-1">
-				<li>First thing</li>
-				<li>Second Thing</li>
+		<div data-role="tabs">
+			<ul>
+				<li><a href="#tab-1" class="">Tab 1</a></li>
+				<li><a href="#tab-2" class="">Tab 2</a></li>
 			</ul>
 
-			<div id="tab-2">
+			<div id="tab-1" data-role="tab-content">
+				<h2>Here is the first tab</h2>
+				<ul data-role="listview">
+					<li>First item.</li>
+					<li>Second item.</li>
+				</ul>
+			</div>
+
+			<div id="tab-2" data-role="tab-content">
 				<h2>Here is the second tab</h2>
+				<p>Second tab content...</p>
 			</div>
 		</div>
 	</div>
--- jquery.mobile.tabs.js
+++ jquery.mobile.tabs.js
@@ -1,101 +1,100 @@
 /*
-* jQuery Mobile Framework : "tabs" plugin
-* https://github.com/groovetrain/jQuery.mobile-Tabs
-*
-* Copyright 2011 (c) Joel Greutman
-* joelgreutman@gmail.com
-* joelgreutman.com
-* Dual licensed under the MIT or GPL Version 2 licenses.
-* http://jquery.org/license
-*/
-(function($, undefined ) {
-$.widget( "mobile.tabs", $.mobile.widget, {
+ * jQuery Mobile Framework : "tabs" plugin
+ * https://github.com/msysh/jQuery.mobile-Tabs
+ *
+ * @author Joel Greutman
+ * @email joelgreutman@gmail.com
+ * @website joelgreutman.com
+ * https://github.com/groovetrain/jQuery.mobile-Tabs
+ *
+ * @author Masayoshi.Ando
+ *
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * 
+ * http://jquery.org/license
+ */
+(function($, undefined) {
+$.widget("mobile.tabs", $.mobile.widget, {
+
 	options: {
 		iconpos: 'top',
 		grid: null,
-		load: function(event, ui) { },
-		beforeTabHide: function(event, ui) { },
-		beforeTabShow: function(event, ui) { },
-		afterTabShow:  function(event, ui) { }
+		load: function(event, ui) {},
+		beforeTabHide: function(event, ui) {},
+		beforeTabShow: function(event, ui) {},
+		afterTabShow:  function(event, ui) {}
 	},
+
 	_create: function(){
-		var
-			$this = this,
-			$tabs = this.element,
-			$navbtns = $tabs.find("a"),
-			iconpos = $navbtns.filter('[data-icon]').length ? this.options.iconpos : undefined;
-		var $content = $tabs.closest('div[data-role="page"]').find('div[data-role="content"]');
+		var $this = this;
+		var $tabs = this.element.find("> ul li a");
+		var $tabContents = this.element.find(':jqmData(role="tab-content")');
+		var iconpos = $tabs.filter('[data-icon]').length ? this.options.iconpos : undefined;
+		var $content = $tabs.closest('div:jqmData(role="page")').find('div:jqmData(role="content")');
 
 		$tabs
-			.addClass('ui-navbar')
-			.attr("role","navigation")
-			.find("ul")
-				.grid({grid: this.options.grid });
-
-		if( !iconpos ){ 
-			$tabs.addClass("ui-navbar-noicons");
-		}
-
-		$navbtns
 			.buttonMarkup({
+				inline: true,
 				corners: false,
 				shadow:  false,
 				iconpos: iconpos
 			})
 			.removeClass('ui-link');
 
-		// Set up the direct children of the page as the tab content, hide them
-		$content.children().addClass('ui-tabs-content');
+		$tabContents.addClass('ui-tabs-content');
 		
-		// Now show the one that's active
-		if( $navbtns.filter('.ui-btn-active').length == 0 )
-			$navbtns.first().addClass('ui-btn-active');
-		$content.children('#' + $navbtns.eq($this.currentTab()).attr('href')).addClass('ui-tabs-content-active');
+		if ($tabs.filter('.ui-btn-active').length == 0){
+			$tabs.first().addClass('ui-btn-active');
+		}
+		$tabContents.filter('#' + $tabs.eq($this.currentTabIndex()).attr('href')).addClass('ui-tabs-content-active');
 
-		$navbtns.bind('click', function(event) {
-			navButtonClick.call(this, event);
-			return false;
-		})
-		.bind('tap', function(event){
-			navButtonClick.call(this, event);
-			return false;
-		});
+		$tabs
+			.bind('click', function(event) {
+				clickTab.call(this, event);
+				return false;
+			})
+			.bind('tap', function(event){
+				clickTab.call(this, event);
+				return false;
+			});
 		
-		function navButtonClick(event) {
-			$navbtns.removeClass( "ui-btn-active" );
-			$( this ).addClass( "ui-btn-active" );
+		function clickTab(event) {
+			$tabs.removeClass("ui-btn-active");
+			$(this).addClass("ui-btn-active");
 			$this.changeTab(event, {
-				currentTab: $navbtns.eq($this.currentTab()),
-				nextTab: $(this),
+				currentTabIndex: $tabs.eq($this.currentTabIndex()),
 				currentContent: $this.currentContent(),
-				nextContent: $content.children($(this).attr('href'))
+				nextTab: $(this),
+				nextContent: $tabContents.filter($(this).attr('href'))
 			});
 			event.preventDefault();
 		}
 
 		this._trigger('load', null, {
-			currentTab: $navbtns.eq($this.currentTab()),
+			currentTabIndex: $tabs.eq($this.currentTabIndex()),
 			currentContent: $this.currentContent()
 		});
 	},
-	currentTab: function() {
-		var $tabs = this.element,
-		$navbtns = $tabs.find("a");
-		return this.element.find('.ui-btn-active').parent().prevAll().length;
+	
+	currentTabIndex: function() {
+		return this.element.find("> ul li a").find('.ui-btn-active').parent().prevAll().length;
 	},
+
 	currentContent: function() {
-		return this.element.closest('div[data-role="page"]').find('div[data-role="content"]').children().filter('.ui-tabs-content-active');
+		return this.element.find('.ui-tabs-content-active');
 	},
+
 	changeTab: function(event, ui) {
-		if( this._trigger('beforeTabHide', event, ui) )
-			ui.currentContent.siblings().andSelf().removeClass('ui-tabs-content-active');
-		if( this._trigger('beforeTabShow', event, ui) )
+		if (this._trigger('beforeTabHide', event, ui)){
+			ui.currentContent.removeClass('ui-tabs-content-active');
+		}
+		if (this._trigger('beforeTabShow', event, ui)){
 			ui.nextContent.addClass('ui-tabs-content-active');
-		this._trigger('afterTabShow', event, $.extend({}, ui, { previousContent: ui.currentContent, currentContent: ui.nextContent, nextContent: null }));
+		}
+		this._trigger('afterTabShow', event, $.extend({}, ui, { 
+															previousContent: ui.currentContent, 
+															currentContent: ui.nextContent, 
+															nextContent: null }));
 	}
 });
 })( jQuery );
-
-$('[data-role=page]').live('pagecreate', function(e) {
-	$(this).find('[data-role="tabs"]').tabs();
-});
